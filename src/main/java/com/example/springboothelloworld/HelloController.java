2fd6b7cdaf7daa1330270d7e7ac4b455c9a32a4c
Mufasa007 <594564264@qq.com>
2021-12-20 00:23:24 +0800

    [CHG] 修改controller

--- src/main/java/com/example/springboothelloworld/HelloController.java
+++ src/main/java/com/example/springboothelloworld/HelloController.java
@@ -5,8 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 /**
  * @Author 59456
  * @Date 2021/12/17
@@ -19,7 +17,13 @@ public class HelloController {
 
     @GetMapping("/hello")
     @ResponseBody
-    public String getJsonDataByCodeList() {
+    public String hello() {
         return "hello world! ";
+    }
+
+    @GetMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "hello world! test";
     }
 }
