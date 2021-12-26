28143075620852e1f10eabdd2f5182ac33d52c93
Mufasa007 <594564264@qq.com>
2021-12-26 22:30:34 +0800

    [CHG]  * 1,使用vector解决，本质就是synchronized(jdk1.0)  * 2,Collections.synchronizedList(new ArrayList<>());  * 3,new CopyOnWriteArrayList<>();

--- src/test/java/concurrent/Eleven/ListTest.java
+++ src/test/java/concurrent/Eleven/ListTest.java
@@ -1,6 +1,7 @@
 package concurrent.Eleven;
 
 import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -11,21 +12,28 @@ import java.util.concurrent.TimeUnit;
  */
 /**
  * 1,使用vector解决，本质就是synchronized(jdk1.0)
+ * 2,Collections.synchronizedList(new ArrayList<>());
+ * 3,new CopyOnWriteArrayList<>();
  */
 public class ListTest {
 
 
     public static void main(String[] args) throws InterruptedException {
-        List<String> stringList = new Vector<>();
-
+//        List<String> stringList = new Vector<>();
+//        List<String> stringList = Collections.synchronizedList(new ArrayList<>());
+        List<String> stringList = new CopyOnWriteArrayList<>();
         for (int i = 0; i < 10; i++) {
             new Thread(()->{
                 stringList.add(UUID.randomUUID().toString().substring(0,5));
                 System.out.println(stringList);
             },String.valueOf(i)).start();
         }
-        stringList.forEach(System.out::println);
+        stringList.forEach(e->{
+            System.out.println(e+"-A");
+        });
         TimeUnit.SECONDS.sleep(4);
-        stringList.forEach(System.out::println);
+        stringList.forEach(e->{
+            System.out.println(e+"-A");
+        });
     }
 }
