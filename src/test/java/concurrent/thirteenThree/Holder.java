743c35ab1e6bc5d2efda915c2d13131e9a977350
Mufasa007 <594564264@qq.com>
2022-01-03 16:47:28 +0800

    [CHG] 33,单例模式 反射可以破坏单例

--- src/test/java/concurrent/thirteenThree/Holder.java
+++ src/test/java/concurrent/thirteenThree/Holder.java
@@ -1,5 +1,8 @@
 package concurrent.thirteenThree;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 /**
  * @Author 59456
  * @Date 2022/1/3
@@ -10,11 +13,21 @@ public class Holder {
     private Holder() {
     }
 
-    public Holder getInstance(){
+    public static Holder getInstance(){
         return InnerClass.holder;
     }
 
     public static class InnerClass{
         private static final Holder holder = new Holder();
+    }
+
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Holder holder0 = getInstance();
+        Constructor<Holder> constructor = Holder.class.getDeclaredConstructor(null);
+        constructor.setAccessible(true);
+        Holder holder1 = constructor.newInstance();
+
+        System.out.println(holder0);
+        System.out.println(holder1);
     }
 }
