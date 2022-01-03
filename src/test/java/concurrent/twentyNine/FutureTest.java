247b633a0ea08e79d84a837d7072847f6cf496b5
Mufasa007 <594564264@qq.com>
2022-01-03 12:10:24 +0800

    [CHG] 29,CompletableFuture.whenComplete

--- src/test/java/concurrent/twentyNine/FutureTest.java
+++ src/test/java/concurrent/twentyNine/FutureTest.java
@@ -24,7 +24,7 @@ public class FutureTest {
 
         // 有返回值的 异步回调
         // 成功或者失败
-        CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(()->{
+/*        CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(()->{
             try {
                 TimeUnit.SECONDS.sleep(2);
             } catch (InterruptedException e) {
@@ -37,6 +37,28 @@ public class FutureTest {
         System.out.println("second");
 
         // 当需要取未来的返回值时，就会阻塞main线程，直到获取到数据
-        System.out.println(completableFuture.get());
+        System.out.println(completableFuture.get());*/
+
+        CompletableFuture<Integer> completableFuture = CompletableFuture.supplyAsync(()->{
+            try {
+                TimeUnit.SECONDS.sleep(2);
+                int sqrt = 10/0;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("one");
+            return 1024;
+        });
+
+        Integer result = completableFuture.whenComplete((t,u)->{
+            System.out.println("t=>" + t); // 正常的返回结果
+            System.out.println("u=>" + u); // 错误信息
+        }) .exceptionally((e)->{
+            e.printStackTrace();
+            return 233;
+        }).get();
+
+        System.out.println("result:"+result);
+
     }
 }
