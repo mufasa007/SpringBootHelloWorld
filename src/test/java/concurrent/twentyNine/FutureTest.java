c8c5720103c1a1076d8a568e13b1a79990c8cf13
Mufasa007 <594564264@qq.com>
2022-01-03 12:00:50 +0800

    [CHG] 29,CompletableFuture.supplyAsync

--- src/test/java/concurrent/twentyNine/FutureTest.java
+++ src/test/java/concurrent/twentyNine/FutureTest.java
@@ -13,13 +13,25 @@ import java.util.concurrent.TimeUnit;
 public class FutureTest {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        CompletableFuture<Void> completableFuture = CompletableFuture.runAsync(()->{
+/*        CompletableFuture<Void> completableFuture = CompletableFuture.runAsync(()->{
             try {
                 TimeUnit.SECONDS.sleep(2);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             System.out.println("one");
+        });*/
+
+        // 有返回值的 异步回调
+        // 成功或者失败
+        CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(()->{
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("one");
+            return "second";
         });
 
         System.out.println("second");
