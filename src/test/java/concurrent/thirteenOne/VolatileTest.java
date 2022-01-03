8f65a3257e36b1082a06a1de48675cd66d76f9ab
Mufasa007 <594564264@qq.com>
2022-01-03 15:00:12 +0800

    [CHG] 31,volatile 可见性测试(方法域使用synchronized)

--- src/test/java/concurrent/thirteenOne/VolatileTest.java
+++ src/test/java/concurrent/thirteenOne/VolatileTest.java
@@ -1,5 +1,7 @@
 package concurrent.thirteenOne;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @Author 59456
  * @Date 2022/1/3
@@ -7,17 +9,18 @@ package concurrent.thirteenOne;
  * @Version 1.0
  */
 public class VolatileTest {
-    private static Integer sum = 0;
-    public static void main(String[] args) {
+    private static  Integer sum = 0;
+    public static void main(String[] args) throws InterruptedException {
         for (int i = 0; i < 10; i++) {
             new Thread(()->{
                 add();
             }).start();
         }
+        TimeUnit.SECONDS.sleep(1);
         System.out.println("sum=>"+sum);
     }
 
-    public static void add(){
+    public synchronized static void add(){
         sum += 1;
     }
 }
