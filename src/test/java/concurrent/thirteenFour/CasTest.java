5324e750db6b9ff514068f2522a357a24fc22e54
Mufasa007 <594564264@qq.com>
2022-01-07 00:04:11 +0800

    [CHG] 34,深入理解CAS ABA问题

--- src/test/java/concurrent/thirteenFour/CasTest.java
+++ src/test/java/concurrent/thirteenFour/CasTest.java
@@ -1,5 +1,6 @@
 package concurrent.thirteenFour;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -10,17 +11,23 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class CasTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         AtomicInteger atomicInteger = new AtomicInteger(2021);
-        System.out.println(atomicInteger.get());
+        System.out.println(Thread.currentThread().getName()+"=>"+atomicInteger.get());
 
         // 期望=>更新 CAS是CPU的并发原语
         atomicInteger.compareAndSet(2021,2022);
-        System.out.println(atomicInteger.get());
+        System.out.println(Thread.currentThread().getName()+"=>"+atomicInteger.get());
+
+        new Thread(()->{
+            atomicInteger.compareAndSet(2022, 2021);
+            System.out.println(Thread.currentThread().getName()+"=>"+atomicInteger.get());
+        }).start();
 
         // 与期望值不符合=>不更新
+        TimeUnit.SECONDS.sleep(1);
         atomicInteger.compareAndSet(2021,2022);
-        System.out.println(atomicInteger.get());
+        System.out.println(Thread.currentThread().getName()+"=>"+atomicInteger.get());
     }
 
 }
