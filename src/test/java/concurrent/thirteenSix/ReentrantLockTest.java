ed2d60dde80a72f8761a1c1401446067542a6495
Mufasa007 <594564264@qq.com>
2022-01-07 22:59:33 +0800

    [CHG] 36,重入锁 ReentrantLock版本可重入锁样例

--- src/test/java/concurrent/thirteenSix/ReentrantLockTest.java
+++ src/test/java/concurrent/thirteenSix/ReentrantLockTest.java
@@ -1,5 +1,8 @@
 package concurrent.thirteenSix;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @Author 59456
  * @Date 2022/1/7
@@ -8,9 +11,35 @@ package concurrent.thirteenSix;
  */
 public class ReentrantLockTest {
     public static void main(String[] args) {
-        Phone phone = new Phone();
+        Phone2 phone = new Phone2();
         new Thread(phone::sms,"A").start();
         new Thread(phone::sms,"B").start();
+    }
+}
+
+class Phone2{
+    Lock lock = new ReentrantLock();
+    public void sms(){
+        lock.lock();
+        try {
+            System.out.println(Thread.currentThread().getName()+"=>"+"发短信");
+            call();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            lock.unlock();
+        }
+    }
+
+    public void call(){
+        lock.lock();
+        try {
+            System.out.println(Thread.currentThread().getName()+"=>"+"打电话");
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            lock.unlock();
+        }
     }
 }
 
