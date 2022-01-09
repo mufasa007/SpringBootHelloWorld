494897bcbadfe9a1545cc9bdb05c52788389c924
Mufasa007 <594564264@qq.com>
2022-01-09 23:36:30 +0800

    [CHG] 3，使用beans.xml注入实体类 没有显式的new对象

--- src/test/java/spring/five/XmlIocTest.java
+++ src/test/java/spring/five/XmlIocTest.java
@@ -13,7 +13,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class XmlIocTest {
     public static void main(String  [] args) {
         // 获取spring的上下文对象
-        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-properties/beans.xml");
         // 我们的对象现在都在Spring中管理了，使用的时候直接取出即可
         Hello hello = (Hello) context.getBean("hello");
         System.out.println(hello);
--- src/test/java/spring/three/IocTest.java
+++ src/test/java/spring/three/IocTest.java
@@ -1,8 +1,8 @@
 package spring.three;
 
-import com.example.springboothelloworld.web.dao.impl.UserDaoMysqlImpl;
 import com.example.springboothelloworld.web.service.UserService;
-import com.example.springboothelloworld.web.service.impl.UserServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * @Author 59456
@@ -12,8 +12,12 @@ import com.example.springboothelloworld.web.service.impl.UserServiceImpl;
  */
 public class IocTest {
     public static void main(String[] args) {
-        UserService userService = new UserServiceImpl();
-        userService.setUserDao(new UserDaoMysqlImpl());
+//        UserService userService = new UserServiceImpl();
+//        userService.setUserDao(new UserDaoMysqlImpl());
+//        userService.getUser();
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-properties/user.xml");
+        UserService userService = (UserService) context.getBean("userService");
         userService.getUser();
     }
 }
